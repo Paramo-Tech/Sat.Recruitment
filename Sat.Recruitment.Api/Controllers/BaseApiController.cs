90122c0888cbd88ad747df08c9dc340ae650b42f
CRuiz_86 <carlos.ruiz@projekt202.com>
2021-08-18 01:10:37 -0500

    Refactoring_PT01 populating BaseApiController and result api

--- Sat.Recruitment.Api/Controllers/BaseApiController.cs
+++ Sat.Recruitment.Api/Controllers/BaseApiController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sat.Recruitment.Application.Core;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Sat.Recruitment.Api.Controllers
 {
@@ -7,6 +11,18 @@ namespace Sat.Recruitment.Api.Controllers
     /// </summary>
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
 
     }
 }
--- Sat.Recruitment.Application/Core/Result.cs
+++ Sat.Recruitment.Application/Core/Result.cs
@@ -4,7 +4,13 @@ namespace Sat.Recruitment.Application.Core
     /// <summary>
     /// generic class to control api responses 
     /// </summary>
-    public class Result
+    public class Result<T>
     {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public string Error { get; set; }
+
+        public static Result<T> Success(T value) => new Result<T> { IsSuccess = true, Value = value };
+        public static Result<T> Failure(string error) => new Result<T> { IsSuccess = false, Error = error };
     }
 }
