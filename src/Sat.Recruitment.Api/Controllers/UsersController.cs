7610917c407eee2e969f35d314c76363ba1d6a8b
adrielairaldo <adrielairaldo@hotmail.com>
2022-02-13 16:56:55 -0300

    Wrap "Normalize email" funcionality, and add descritive comments to the every line

--- src/Sat.Recruitment.Api/Controllers/UsersController.cs
+++ src/Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -79,14 +79,48 @@ namespace Sat.Recruitment.Api.Controllers
 
             var reader = ReadUsersFromFile();
 
-            //Normalize email
+            #region Normalize email
+
+            /* After some quick research, I discovered that the use of the . and the + are
+             * Gmail-specific implementations, and that there is no standard to support them,
+             * so implementing the "Normalize Email" functionality to all email providers
+             * could lead to the generation of invalid addresses.
+             * 
+             * An official source on Google Blog:
+             *    https://gmail.googleblog.com/2008/03/2-hidden-ways-to-get-more-from-your.html
+             *    
+             * Then, the location of the dots matters for emails on Microsoft Outlook,
+             * Yahoo Mail, and Apple iCloud, to mention some. Dots don’t matter for Facebook,
+             * and they aren’t used at all for Twitter handles.
+             */
+
+            /* Takes an string and splits it on the @ character, if any of the partitions
+             * result in an empty array, it removes them from the result.
+             * 
+             * Important fact here: if the supplied string is not a string of type
+             * somestring@anotherstring the algorithm will fail at runtime without exception
+             * handling.
+             */
             var aux = newUser.Email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
 
+            /* Finds the location of the first occurrence of the + sign, and returns it
+             * in with respect to a zero-based array. If the sign is not found, then return -1.
+             */
             var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
 
+            /* If the string does not contain a + sign: then it replaces occurrences of the
+             * sign . with empty spaces.
+             * 
+             * If the string does contain a + sign: then it replaces occurrences of the sign .
+             * with empty spaces, and remove everything to the right of the + sign (including
+             * the + sign)
+             * */
             aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
 
+            // Compose the email again with the two parts of the chain.
             newUser.Email = string.Join("@", new string[] { aux[0], aux[1] });
+
+            #endregion // Normalize email
 
             while (reader.Peek() >= 0)
             {
