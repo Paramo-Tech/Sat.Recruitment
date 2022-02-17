2eab777ab41b6f677bb7655aaf7f222910fd5c51
adrielairaldo <adrielairaldo@hotmail.com>
2022-02-17 11:54:35 -0300

    Move the logic that maps a file row to a User entity to a specific private method.

--- src/Sat.Recruitment.Infrastructure/Repositories/UserRepository.cs
+++ src/Sat.Recruitment.Infrastructure/Repositories/UserRepository.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace Sat.Recruitment.Infrastructure.Repositories
 {
@@ -25,42 +24,10 @@ namespace Sat.Recruitment.Infrastructure.Repositories
                 while (reader.Peek() >= 0)
                 {
                     // Read a line
-                    var line = reader.ReadLineAsync().Result;
+                    string line = reader.ReadLineAsync().Result;
 
-                    // Split a line by the , symbol separator
-                    string[] lineColumns = line.Split(new char[] { ',' });
-
-                    if (lineColumns.Length != 6)
-                    {
-                        throw new CorruptRegistryException(line, $"Amount of columns: {lineColumns.Length}");
-                    }
-
-                    // Pass each row column to a corresponding field name
-                    string name = lineColumns[0];
-                    string email = lineColumns[1];
-                    string phone = lineColumns[2];
-                    string address = lineColumns[3];
-                    string userType = lineColumns[4];
-                    string money = lineColumns[5];
-
-                    // Fiel a User entity with the corresponding fields, and do the corresponding conversion in case is needed
-                    var user = new User();
-                    try
-                    {
-                        // TODO: Throw specific exceptions for each field that is parsed (Email with a correct pattern,
-                        // UserType with an integer belonging to the Enum, etc).
-
-                        user.Name = name;
-                        user.Email = email;
-                        user.Phone = phone;
-                        user.Address = address;
-                        user.UserType = (string.IsNullOrEmpty(userType)) ? null : (UserType?)Int32.Parse(userType);
-                        user.Money = decimal.Parse(money);
-                    }
-                    catch (Exception)
-                    {
-                        throw new CorruptRegistryException(line, "Error occurred in data parsing.");
-                    }
+                    // Map line to User
+                    User user = MapFileRowToUser(line);
 
                     users.Add(user);
                 }
@@ -75,6 +42,50 @@ namespace Sat.Recruitment.Infrastructure.Repositories
             }
 
             return users;
+        }
+
+        /// <summary>
+        /// Given a file row, fill each of a User entity properties with each
+        /// corresponding column of the row.
+        /// </summary>
+        private User MapFileRowToUser(string fileRow)
+        {
+            // Split a line by the , symbol separator
+            string[] lineColumns = fileRow.Split(new char[] { ',' });
+
+            if (lineColumns.Length != 6)
+            {
+                throw new CorruptRegistryException(fileRow, $"Amount of columns: {lineColumns.Length}");
+            }
+
+            // Pass each row column to a corresponding field name
+            string name = lineColumns[0];
+            string email = lineColumns[1];
+            string phone = lineColumns[2];
+            string address = lineColumns[3];
+            string userType = lineColumns[4];
+            string money = lineColumns[5];
+
+            // Fiel a User entity with the corresponding fields, and do the corresponding conversion in case is needed
+            var user = new User();
+            try
+            {
+                // TODO: Throw specific exceptions for each field that is parsed (Email with a correct pattern,
+                // UserType with an integer belonging to the Enum, etc).
+
+                user.Name = name;
+                user.Email = email;
+                user.Phone = phone;
+                user.Address = address;
+                user.UserType = (string.IsNullOrEmpty(userType)) ? null : (UserType?)Int32.Parse(userType);
+                user.Money = decimal.Parse(money);
+            }
+            catch (Exception)
+            {
+                throw new CorruptRegistryException(fileRow, "Error occurred in data parsing.");
+            }
+
+            return user;
         }
     }
 }
