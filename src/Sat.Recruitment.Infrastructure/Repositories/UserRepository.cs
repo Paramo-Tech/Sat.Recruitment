663bbd2553c1069bbb092918b014676474ef35c5
adrielairaldo <adrielairaldo@hotmail.com>
2022-02-17 11:45:55 -0300

    Delete ReadUsersFromFile method, and add that functionality inside GetAll(...). The reason is that is posible to use reader.Dispose() instead of using, but it will not close file/reader/writer in case of exceptions.

--- src/Sat.Recruitment.Infrastructure/Repositories/UserRepository.cs
+++ src/Sat.Recruitment.Infrastructure/Repositories/UserRepository.cs
@@ -16,73 +16,65 @@ namespace Sat.Recruitment.Infrastructure.Repositories
         {
             List<User> users = new List<User>();
 
-            var reader = ReadUsersFromFile();
+            #region Get users from file
 
-            #region Get users from storage
-
-            while (reader.Peek() >= 0)
+            string path = AppContext.BaseDirectory + "/Files/Users.txt";
+            using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            using (StreamReader reader = new StreamReader(fileStream))
             {
-                // Read a line
-                var line = reader.ReadLineAsync().Result;
-
-                // Split a line by the , symbol separator
-                string[] lineColumns = line.Split(new char[] { ',' });
-
-                if (lineColumns.Length != 6)
+                while (reader.Peek() >= 0)
                 {
-                    throw new CorruptRegistryException(line, $"Amount of columns: {lineColumns.Length}");
+                    // Read a line
+                    var line = reader.ReadLineAsync().Result;
+
+                    // Split a line by the , symbol separator
+                    string[] lineColumns = line.Split(new char[] { ',' });
+
+                    if (lineColumns.Length != 6)
+                    {
+                        throw new CorruptRegistryException(line, $"Amount of columns: {lineColumns.Length}");
+                    }
+
+                    // Pass each row column to a corresponding field name
+                    string name = lineColumns[0];
+                    string email = lineColumns[1];
+                    string phone = lineColumns[2];
+                    string address = lineColumns[3];
+                    string userType = lineColumns[4];
+                    string money = lineColumns[5];
+
+                    // Fiel a User entity with the corresponding fields, and do the corresponding conversion in case is needed
+                    var user = new User();
+                    try
+                    {
+                        // TODO: Throw specific exceptions for each field that is parsed (Email with a correct pattern,
+                        // UserType with an integer belonging to the Enum, etc).
+
+                        user.Name = name;
+                        user.Email = email;
+                        user.Phone = phone;
+                        user.Address = address;
+                        user.UserType = (string.IsNullOrEmpty(userType)) ? null : (UserType?)Int32.Parse(userType);
+                        user.Money = decimal.Parse(money);
+                    }
+                    catch (Exception)
+                    {
+                        throw new CorruptRegistryException(line, "Error occurred in data parsing.");
+                    }
+
+                    users.Add(user);
                 }
-
-                // Pass each row column to a corresponding field name
-                string name = lineColumns[0];
-                string email = lineColumns[1];
-                string phone = lineColumns[2];
-                string address = lineColumns[3];
-                string userType = lineColumns[4];
-                string money = lineColumns[5];
-
-                // Fiel a User entity with the corresponding fields, and do the corresponding conversion in case is needed
-                var user = new User();
-                try
-                {
-                    // TODO: Throw specific exceptions for each field that is parsed (Email with a correct pattern,
-                    // UserType with an integer belonging to the Enum, etc).
-
-                    user.Name = name;
-                    user.Email = email;
-                    user.Phone = phone;
-                    user.Address = address;
-                    user.UserType = (string.IsNullOrEmpty(userType)) ? null : (UserType?)Int32.Parse(userType);
-                    user.Money = decimal.Parse(money);
-                }
-                catch (Exception)
-                {
-                    throw new CorruptRegistryException(line, "Error occurred in data parsing.");
-                }
-
-                users.Add(user);
             }
-            reader.Close();
 
-            #endregion // Get users from storage
+            #endregion // Get users from file
 
             // If filter parameter is not null, apply it to the results
             if (filter != null)
             {
-               users = users.Where(filter).ToList();
+                users = users.Where(filter).ToList();
             }
 
             return users;
-        }
-
-        private StreamReader ReadUsersFromFile()
-        {
-            var path = AppContext.BaseDirectory + "/Files/Users.txt";
-
-            FileStream fileStream = new FileStream(path, FileMode.Open);
-
-            StreamReader reader = new StreamReader(fileStream);
-            return reader;
         }
     }
 }
