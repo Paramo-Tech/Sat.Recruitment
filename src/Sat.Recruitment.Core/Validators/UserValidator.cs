4f7070abdae8098d539920376c6e7276584d5fc9
adrielairaldo <adrielairaldo@hotmail.com>
2022-02-21 19:31:24 -0300

    Add a validator for the User business entity.

---
+++ src/Sat.Recruitment.Core/Validators/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Sat.Recruitment.Core.DomainEntities;
+using System.Text.RegularExpressions;
+
+namespace Sat.Recruitment.Core.Validators
+{
+    internal class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            // Name
+            RuleFor(user => user.Name).NotEmpty().WithMessage("The name is required");
+
+            // Email
+            RuleFor(user => user.Email).NotEmpty().EmailAddress().WithMessage("The email is required");
+
+            // Address
+            RuleFor(user => user.Address).NotEmpty().WithMessage("The address is required");
+
+            // Phone
+            RuleFor(user => user.Phone).NotEmpty().Matches(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}").WithMessage("The phone is required");
+        }
+    }
+}
