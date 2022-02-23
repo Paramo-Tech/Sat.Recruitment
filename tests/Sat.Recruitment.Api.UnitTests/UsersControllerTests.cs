01b94c0ee7b1a267502be15eb06e0dceb46a4ce0
adrielairaldo <adrielairaldo@hotmail.com>
2022-02-22 21:50:24 -0300

    Add Create_WithUnexistingUser_ReturnsCreatedAtActionResultWithCreateResponse test to UserServiceWithTextFile.

--- tests/Sat.Recruitment.Api.UnitTests/UsersControllerTests.cs
+++ tests/Sat.Recruitment.Api.UnitTests/UsersControllerTests.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Sat.Recruitment.Api.Controllers;
+using Sat.Recruitment.Api.DTOs.Users;
 using Sat.Recruitment.Api.MappingProfiles;
 using Sat.Recruitment.Core.Abstractions.Services;
 using Sat.Recruitment.Core.DomainEntities;
+using Sat.Recruitment.Core.Enums;
 using Sat.Recruitment.Core.Exceptions;
 using System;
 using System.Threading.Tasks;
@@ -29,6 +31,62 @@ namespace Sat.Recruitment.Api.UnitTests
                 cfg.AddProfile(new DomainEntitiesMappingProfile());
             });
             _mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Create_WithUnexistingUser_ReturnsCreatedAtActionResultWithCreateResponse()
+        {
+            // Arrange
+            User createdUser = new User() // Irrelevant content, but needs to be correct
+            {
+                Id = Guid.Parse("999e0cf1-d9e0-498f-ba42-8a9a8f402459"),
+                Name = "Testing User",
+                Email = "testinguser@domain.com",
+                Address = "Testing User 123",
+                Phone = "+54123123123",
+                UserType = UserType.Normal,
+                Money = 100
+            };
+
+            CreateRequest createRequest = new CreateRequest() // Irrelevant content, but needs to be correct
+            {
+                Name = "Testing User",
+                Email = "testinguser@domain.com",
+                Address = "Testing User 123",
+                Phone = "+54123123123",
+                UserType = UserType.Normal,
+                Money = 100
+            };
+
+            CreateResponse expectedCreateResponse = new CreateResponse() // Irrelevant content, but needs to be correct
+            {
+                Id = Guid.Parse("999e0cf1-d9e0-498f-ba42-8a9a8f402459"),
+                Name = "Testing User",
+                Email = "testinguser@domain.com",
+                Address = "Testing User 123",
+                Phone = "+54123123123",
+                UserType = UserType.Normal,
+                Money = 100
+            };
+
+            var userServiceStub = new Mock<IUserService>();
+            userServiceStub.Setup(userService => userService.Create(It.IsAny<User>())).ReturnsAsync(createdUser);
+
+            var controller = new UsersController(_logger, _mapper, userServiceStub.Object);
+
+            // Act
+            var result = await controller.Create(createRequest);
+            CreateResponse obtainedCreateResponse = (result.Result as CreatedAtActionResult).Value as CreateResponse;
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.Equal(expectedCreateResponse.Id, obtainedCreateResponse.Id);
+            Assert.Equal(expectedCreateResponse.Name, obtainedCreateResponse.Name);
+            Assert.Equal(expectedCreateResponse.Email, obtainedCreateResponse.Email);
+            Assert.Equal(expectedCreateResponse.Address, obtainedCreateResponse.Address);
+            Assert.Equal(expectedCreateResponse.Phone, obtainedCreateResponse.Phone);
+            Assert.Equal(expectedCreateResponse.UserType, obtainedCreateResponse.UserType);
+            Assert.Equal(expectedCreateResponse.Money, obtainedCreateResponse.Money);
         }
 
         [Fact]
