98857e047147a57e83326ddeed50c404cf6e81fa
Franco Redoni <lennarx@outlook.com>
2022-03-21 22:39:07 -0300

    Auth Validator

---
+++ Sat.Recruitment.Api/Validators/AuthValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Sat.Recruitment.Api.Constants;
+using Sat.Recruitment.Domain.Forms;
+
+namespace Sat.Recruitment.Api.Validators
+{
+    public class AuthValidator : AbstractValidator<LoginForm>
+    {
+        public AuthValidator()
+        {
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage(Messages.PasswordErrorRequired);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage(Messages.EmailErrorRequired);
+
+        }
+    }
+}
