1a1622988f0ba6a884c40301f9e15568684dfec1
martin.cuba <martin.cuba459@gmail.com>
2022-06-11 20:31:44 -0300

    fix how to assert a boolean type

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace Sat.Recruitment.Test
             var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
 
-            Assert.Equal(true, result.IsSuccess);
+            Assert.True(result.IsSuccess);
             Assert.Equal("User Created", result.Errors);
         }
 
@@ -26,7 +26,7 @@ namespace Sat.Recruitment.Test
             var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
 
-            Assert.Equal(false, result.IsSuccess);
+            Assert.False(result.IsSuccess);
             Assert.Equal("The user is duplicated", result.Errors);
         }
     }
