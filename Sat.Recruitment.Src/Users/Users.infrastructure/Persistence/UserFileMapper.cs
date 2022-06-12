dee5d7a8d00e4241039883493fd59a682fcd9b58
martin.cuba <martin.cuba459@gmail.com>
2022-06-12 20:53:55 -0300

    implement user persistence layer

---
+++ Sat.Recruitment.Src/Users/Users.infrastructure/Persistence/UserFileMapper.cs
@@ -0,0 +1,17 @@
+﻿using Users.Domain;
+
+namespace Users.infrastructure.Persistence
+{
+    public static class UserFileMapper
+    {
+        public static User Execute(string line) => new()
+        {
+            Name = line.Split(',')[0].ToString(),
+            Email = new(line.Split(',')[1].ToString()),
+            Phone = new(line.Split(',')[2].ToString()),
+            Address = line.Split(',')[3].ToString(),
+            UserType = UserType.FromValue(line.Split(',')[4].ToString()),
+            Money = decimal.Parse(line.Split(',')[5].ToString()),
+        };
+    }
+}
---
+++ Sat.Recruitment.Src/Users/Users.infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Users.Domain;
+
+namespace Users.infrastructure.Persistence
+{
+    public class UserRepository : IUserRepository
+    {
+        public void save(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> Search(User user)
+        {
+            var users = await this.GetAllAsync();
+            foreach (var saveUser in users)
+            {
+                if (saveUser.Email.Equals(user.Email) || saveUser.Phone.Equals(user.Phone))
+                {
+                    return saveUser;
+                }
+                else if (saveUser.Name.Equals(user.Name) && saveUser.Address.Equals(user.Address))
+                {
+                    return saveUser;
+                }
+            }
+
+            return null;
+        }
+
+        private async Task<List<User>> GetAllAsync()
+        {
+            var path = Directory.GetCurrentDirectory() + "/Files/Users.txt";
+            FileStream fileStream = new(path, FileMode.Open);
+            StreamReader reader = new(fileStream);
+
+            List<User> users = new ();
+
+            while (reader.Peek() >= 0)
+            {
+                var line = await reader.ReadLineAsync();
+                var user = UserFileMapper.Execute(line);
+                users.Add(user);
+            }
+            reader.Close();
+
+            return users;
+        }
+    }
+}
