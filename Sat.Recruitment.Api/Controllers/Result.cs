8c138de9491ad87a0af2204298637dd428be8eaa
martin.cuba <martin.cuba459@gmail.com>
2022-06-11 21:17:36 -0300

    Clean create user method

---
+++ Sat.Recruitment.Api/Controllers/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace Sat.Recruitment.Api.Controllers
+{
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+        public string Errors { get; set; }
+    }
+}
--- Sat.Recruitment.Api/Controllers/UsersController.cs
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 
 namespace Sat.Recruitment.Api.Controllers
 {
-    public class Result
-    {
-        public bool IsSuccess { get; set; }
-        public string Errors { get; set; }
-    }
-
     [ApiController]
     [Route("[controller]")]
     public partial class UsersController : ControllerBase
     {
 
         private readonly List<User> _users = new List<User>();
+
         public UsersController()
         {
         }
@@ -31,11 +26,13 @@ namespace Sat.Recruitment.Api.Controllers
             ValidateErrors(name, email, address, phone, ref errors);
 
             if (errors != null && errors != "")
+            {
                 return new Result()
                 {
                     IsSuccess = false,
                     Errors = errors
                 };
+            }
 
             var newUser = new User
             {
@@ -47,112 +44,12 @@ namespace Sat.Recruitment.Api.Controllers
                 Money = decimal.Parse(money)
             };
 
-            if (newUser.UserType == "Normal")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var percentage = Convert.ToDecimal(0.12);
-                    //If new user is normal and has more than USD100
-                    var gif = decimal.Parse(money) * percentage;
-                    newUser.Money = newUser.Money + gif;
-                }
-                if (decimal.Parse(money) < 100)
-                {
-                    if (decimal.Parse(money) > 10)
-                    {
-                        var percentage = Convert.ToDecimal(0.8);
-                        var gif = decimal.Parse(money) * percentage;
-                        newUser.Money = newUser.Money + gif;
-                    }
-                }
-            }
-            if (newUser.UserType == "SuperUser")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var percentage = Convert.ToDecimal(0.20);
-                    var gif = decimal.Parse(money) * percentage;
-                    newUser.Money = newUser.Money + gif;
-                }
-            }
-            if (newUser.UserType == "Premium")
-            {
-                if (decimal.Parse(money) > 100)
-                {
-                    var gif = decimal.Parse(money) * 2;
-                    newUser.Money = newUser.Money + gif;
-                }
-            }
+            newUser.Money += CalculateUserGif(newUser.UserType, decimal.Parse(money));
+            newUser.Email = NormalizeEmail(newUser.Email);
 
-
-            var reader = ReadUsersFromFile();
-
-            //Normalize email
-            var aux = newUser.Email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
-
-            var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
-
-            aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
-
-            newUser.Email = string.Join("@", new string[] { aux[0], aux[1] });
-
-            while (reader.Peek() >= 0)
-            {
-                var line = reader.ReadLineAsync().Result;
-                var user = new User
-                {
-                    Name = line.Split(',')[0].ToString(),
-                    Email = line.Split(',')[1].ToString(),
-                    Phone = line.Split(',')[2].ToString(),
-                    Address = line.Split(',')[3].ToString(),
-                    UserType = line.Split(',')[4].ToString(),
-                    Money = decimal.Parse(line.Split(',')[5].ToString()),
-                };
-                _users.Add(user);
-            }
-            reader.Close();
             try
             {
-                var isDuplicated = false;
-                foreach (var user in _users)
-                {
-                    if (user.Email == newUser.Email
-                        ||
-                        user.Phone == newUser.Phone)
-                    {
-                        isDuplicated = true;
-                    }
-                    else if (user.Name == newUser.Name)
-                    {
-                        if (user.Address == newUser.Address)
-                        {
-                            isDuplicated = true;
-                            throw new Exception("User is duplicated");
-                        }
-
-                    }
-                }
-
-                if (!isDuplicated)
-                {
-                    Debug.WriteLine("User Created");
-
-                    return new Result()
-                    {
-                        IsSuccess = true,
-                        Errors = "User Created"
-                    };
-                }
-                else
-                {
-                    Debug.WriteLine("The user is duplicated");
-
-                    return new Result()
-                    {
-                        IsSuccess = false,
-                        Errors = "The user is duplicated"
-                    };
-                }
+                ValidateUserAlreadyExists(newUser);            
             }
             catch
             {
@@ -163,6 +60,13 @@ namespace Sat.Recruitment.Api.Controllers
                     Errors = "The user is duplicated"
                 };
             }
+            Debug.WriteLine("User Created");
+
+            return new Result()
+            {
+                IsSuccess = true,
+                Errors = "User Created"
+            };
         }
 
         //Validate errors
@@ -181,14 +85,97 @@ namespace Sat.Recruitment.Api.Controllers
                 //Validate if Phone is null
                 errors = errors + " The phone is required";
         }
-    }
-    public class User
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
-        public string UserType { get; set; }
-        public decimal Money { get; set; }
+
+        private string NormalizeEmail (string email)
+        {
+            var aux = email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
+
+            aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
+
+            return string.Join("@", new string[] { aux[0], aux[1] });
+        }
+
+        private void LoadUserList()
+        {
+            var reader = ReadUsersFromFile();
+
+            while (reader.Peek() >= 0)
+            {
+                var line = reader.ReadLineAsync().Result;
+                var user = new User
+                {
+                    Name = line.Split(',')[0].ToString(),
+                    Email = line.Split(',')[1].ToString(),
+                    Phone = line.Split(',')[2].ToString(),
+                    Address = line.Split(',')[3].ToString(),
+                    UserType = line.Split(',')[4].ToString(),
+                    Money = decimal.Parse(line.Split(',')[5].ToString()),
+                };
+                _users.Add(user);
+            }
+            reader.Close();
+        }
+
+        private decimal CalculateUserGif(string userType, decimal currentMoney)
+        {
+            var gif = decimal.Zero;
+            if (userType == "Normal")
+            {
+                if (currentMoney > 100)
+                {
+                    var percentage = Convert.ToDecimal(0.12);
+                    //If new user is normal and has more than USD100
+                    gif = currentMoney * percentage;
+                }
+                if (currentMoney < 100)
+                {
+                    if (currentMoney > 10)
+                    {
+                        var percentage = Convert.ToDecimal(0.8);
+                        gif = currentMoney * percentage;
+                    }
+                }
+            }
+            if (userType == "SuperUser")
+            {
+                if (currentMoney > 100)
+                {
+                    var percentage = Convert.ToDecimal(0.20);
+                    gif = currentMoney * percentage;
+                }
+            }
+            if (userType == "Premium")
+            {
+                if (currentMoney > 100)
+                {
+                    gif = currentMoney * 2;
+                }
+            }
+
+            return gif;
+        }
+
+        private void ValidateUserAlreadyExists(User newUser)
+        {
+            LoadUserList();
+            foreach (var user in _users)
+            {
+                if (user.Email == newUser.Email
+                    ||
+                    user.Phone == newUser.Phone)
+                {
+                    throw new Exception("User is duplicated");
+                }
+                else if (user.Name == newUser.Name)
+                {
+                    if (user.Address == newUser.Address)
+                    {
+                        throw new Exception("User is duplicated");
+                    }
+                }
+            }
+        }
     }
 }
