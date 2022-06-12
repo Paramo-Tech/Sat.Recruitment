52e485103d4b9009ae6605a182557acac88c0a21
martin.cuba <martin.cuba459@gmail.com>
2022-06-12 20:50:15 -0300

    new customs exceptions for domain and application layers

---
+++ Sat.Recruitment.Src/Shared/Shared.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string? message) : base(message)
+        {
+        }
+    }
+}
