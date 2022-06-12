3c497bf99c3ec05273eea30c3204d51e735abb14
martin.cuba <martin.cuba459@gmail.com>
2022-06-12 20:50:44 -0300

    implement enumeration

---
+++ Sat.Recruitment.Src/Shared/Shared.Domain/Enumeration.cs
@@ -0,0 +1,97 @@
+﻿using System.Reflection;
+
+namespace Shared.Domain
+{
+    public abstract class Enumeration : IComparable
+    {
+        private readonly string value;
+        private readonly string displayName;
+
+        protected Enumeration(string value, string displayName)
+        {
+            this.value = value;
+            this.displayName = displayName;
+        }
+
+        public string Value
+        {
+            get { return this.value; }
+        }
+
+        public string DisplayName
+        {
+            get { return this.displayName; }
+        }
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration, new()
+        {
+            var type = typeof(T);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            foreach (var info in fields)
+            {
+                var instance = new T();
+
+                if (info.GetValue(instance) is T locatedValue)
+                {
+                    yield return locatedValue;
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+
+            if (otherValue == null)
+            {
+                return false;
+            }
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = this.value.Equals(otherValue.Value);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.value.GetHashCode();
+        }
+
+        public static T FromValue<T>(string value) where T : Enumeration, new()
+        {
+            var matchingItem = parse<T, string>(value, "value", item => item.Value == value);
+            return matchingItem;
+        }
+
+        public static T FromDisplayName<T>(string displayName) where T : Enumeration, new()
+        {
+            var matchingItem = parse<T, string>(displayName, "display name", item => item.DisplayName == displayName);
+            return matchingItem;
+        }
+
+        private static T parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration, new()
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+            {
+                var message = string.Format("'{0}' is not a valid {1} in {2}", value, description, typeof(T));
+                throw new ApplicationException(message);
+            }
+
+            return matchingItem;
+        }
+
+        public int CompareTo(object other)
+        {
+            return Value.CompareTo(((Enumeration)other).Value);
+        }
+    }
+}
