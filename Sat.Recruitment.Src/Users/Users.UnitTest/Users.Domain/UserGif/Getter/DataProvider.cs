e79b231e9c54fef8162f9345ad4d5c86d5b45a97
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 10:39:02 -0300

    testfor git calculate getter

---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Users.Domain/UserGif/Getter/DataProvider.cs
@@ -0,0 +1,15 @@
+﻿using Users.Domain;
+using Users.Domain.UserGif.Calculators;
+
+namespace Users.UnitTest.Users.Domain.UserGif.Getter
+{
+    public static class DataProvider
+    {
+        public static IEnumerable<object[]> Calculators()
+        {
+            yield return new object[] { UserType.Normal, new CalculateNormalUserGif() };
+            yield return new object[] { UserType.Premium, new CalculatePremiunUserGif() };
+            yield return new object[] { UserType.SuperUser, new CalculateSuperUserGif() };
+        }
+    }
+}
---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Users.Domain/UserGif/Getter/GifCalculateGetterTest.cs
@@ -0,0 +1,23 @@
+﻿using Users.Domain;
+using Users.Domain.UserGif.Calculators;
+using Users.Domain.UserGif.Getter;
+using Xunit;
+
+namespace Users.UnitTest.Users.Domain.UserGif.Getter
+{
+    public class GifCalculateGetterTest
+    {
+        private readonly GifCalculateGetter gifCalculateGetter = new();
+
+        [Theory]
+        [MemberData(nameof(DataProvider.Calculators), MemberType = typeof(DataProvider))]
+        public void Handle_WhenCommandIsInvalid_ExceptionExpected(
+            UserType userType,
+            ICalculateUserGif expectedCalculateUserGif)
+        {
+            var result = this.gifCalculateGetter.GetCalculator(userType);
+
+            Assert.IsType(expectedCalculateUserGif.GetType(), result);
+        }
+    }
+}
