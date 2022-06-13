10c96f78072ea94ee8457568a96b93ddfe102b06
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 17:41:11 -0300

    add test for existing user specification

---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Users.Domain/Specifications/DataProvider.cs
@@ -0,0 +1,98 @@
+﻿using Users.Domain;
+using Users.Domain.UserGif.Calculators;
+
+namespace Users.UnitTest.Users.Domain.Specifications
+{
+    public static class DataProvider
+    {
+        public static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[] { BaseUser(), BaseUser(), true };
+            yield return new object[] { BaseUser(), OtherNameThanBase(), true };
+            yield return new object[] { BaseUser(), OtherEmailThanBase(), true };
+            yield return new object[] { BaseUser(), OnlySameEmailThanBase(), true };
+            yield return new object[] { BaseUser(), OnlySamePhoneThanBase(), true };
+            yield return new object[] { BaseUser(), SameNameAndAddressThanBase(), true };
+            yield return new object[] { BaseUser(), DifferentThanBase(), false };
+            yield return new object[] { BaseUser(), OnlySameNameThanBase(), false };
+            yield return new object[] { BaseUser(), OnlySameAddressThanBase(), false };
+        }
+
+        private static User BaseUser() => new()
+        {
+            Name = "Mike",
+            Email = new("mike@gmail.com"),
+            Address = "Av. Juan G",
+            Phone = new("+349 1122354215"),
+            UserType = UserType.Normal,
+            Money = 124
+        };
+
+        private static User DifferentThanBase() => new()
+        {
+            Name = "Cuba",
+            Email = new("cuba@gmail.com"),
+            Address = "Other street",
+            Phone = new("+349 1122354216"),
+            UserType = UserType.Normal,
+            Money = 124
+        };
+
+        private static User OtherNameThanBase()
+        {
+            var originalUser = BaseUser();
+            originalUser.Name = DifferentThanBase().Name;
+
+            return originalUser;
+        }
+
+        private static User OtherEmailThanBase()
+        {
+            var originalUser = BaseUser();
+            originalUser.Email = DifferentThanBase().Email;
+
+            return originalUser;
+        }
+
+        private static User OnlySameEmailThanBase()
+        {
+            var differentUser = DifferentThanBase();
+            differentUser.Email = BaseUser().Email;
+
+            return differentUser;
+        }
+
+        private static User OnlySamePhoneThanBase()
+        {
+            var differentUser = DifferentThanBase();
+            differentUser.Phone = BaseUser().Phone;
+
+            return differentUser;
+        }
+
+        private static User SameNameAndAddressThanBase()
+        {
+            var differentUser = DifferentThanBase();
+            differentUser.Name = BaseUser().Name;
+            differentUser.Address = BaseUser().Address;
+
+            return differentUser;
+        }
+
+        private static User OnlySameNameThanBase()
+        {
+            var differentUser = DifferentThanBase();
+            differentUser.Name = BaseUser().Name;
+
+            return differentUser;
+        }
+
+        private static User OnlySameAddressThanBase()
+        {
+            var differentUser = DifferentThanBase();
+            differentUser.Address = BaseUser().Address;
+
+            return differentUser;
+        }
+    }
+}
---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Users.Domain/Specifications/ExistingUserSpecificationTest.cs
@@ -0,0 +1,27 @@
+﻿using Users.Domain;
+using Users.Domain.Specifications;
+using Xunit;
+
+namespace Users.UnitTest.Users.Domain.Specifications
+{
+    public class ExistingUserSpecificationTest
+    {
+        [Theory]
+        [MemberData(nameof(DataProvider.TestCases), MemberType = typeof(DataProvider))]
+        public void IsSatisfied_MasiveCasesWithExpectedResult(
+            User user,
+            User userToCompare,
+            bool expectedResult)
+        {
+            ExistingUserSpecification specification = new (
+                user.Name,
+                user.Email,
+                user.Address,
+                user.Phone);
+
+            bool result = specification.IsSatisfied(userToCompare);
+
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
