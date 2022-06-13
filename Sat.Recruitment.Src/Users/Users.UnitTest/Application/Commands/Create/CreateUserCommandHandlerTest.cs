e531d9479ca1bff8bfd58272d8ce5dded020d8c6
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 10:17:46 -0300

    test create user command handler

---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Application/Commands/Create/CreateUserCommandHandlerTest.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using Shared.Domain;
+using Users.Application.Commands.Create;
+using Users.Domain;
+using Users.Domain.UserGif.Calculators;
+using Users.Domain.UserGif.Getter;
+using Xunit;
+
+namespace Users.UnitTest.Application.Commands.Create
+{
+    public class CreateUserCoammndHandlerTest
+    {
+        private readonly Mock<IGifCalculateGetter> gifCalculateGetterMock = new();
+        private readonly Mock<IUserRepository> userRepositoryMock = new();
+        private CreateUserCommandHandler createUserCoammndHandler;
+
+        public CreateUserCoammndHandlerTest()
+        {
+            this.gifCalculateGetterMock
+                .Setup(x => x.GetCalculator(It.IsAny<UserType>()))
+                .Verifiable();
+
+            this.userRepositoryMock
+                .Setup(x => x.Search(It.IsAny<ISpecification<User>>()))
+                .Verifiable();
+
+            this.createUserCoammndHandler = new(
+                this.gifCalculateGetterMock.Object,
+                this.userRepositoryMock.Object);
+        }
+
+        [Theory]
+        [MemberData(nameof(DataProvider.InvalidCommands), MemberType = typeof(DataProvider))]
+        public void Handle_WhenCommandIsInvalid_ExceptionExpected(CreateUserCommand command)
+        {
+            Assert.ThrowsAsync<ApplicationException>(
+              () => this.createUserCoammndHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public void Handle_WhenCommandIsValidAndUserAlreadyExists_ExceptionExpected()
+        {
+            this.userRepositoryMock
+                .Setup(x => x.Search(It.IsAny<ISpecification<User>>()))
+                .ReturnsAsync(new User());
+
+            var validCommand = DataProvider.ValidCommand();
+
+            Assert.ThrowsAsync<ApplicationException>(
+               () =>  this.createUserCoammndHandler.Handle(validCommand, CancellationToken.None));
+
+            this.userRepositoryMock
+                .Verify(x => x.Search(It.IsAny<ISpecification<User>>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Handle_WhenCommandIsValidAndUserNotExists_Success()
+        {
+            this.gifCalculateGetterMock
+                .Setup(x => x.GetCalculator(It.IsAny<UserType>()))
+                .Returns(new CalculateNormalUserGif());
+
+            this.userRepositoryMock
+                .Setup(x => x.Search(It.IsAny<ISpecification<User>>()))
+                .ReturnsAsync(() => null);
+
+            var validCommand = DataProvider.ValidCommand();
+
+            await this.createUserCoammndHandler.Handle(validCommand, CancellationToken.None);
+
+            this.gifCalculateGetterMock
+                .Verify(x => x.GetCalculator(It.IsAny<UserType>()), Times.Once());
+            this.userRepositoryMock
+                .Verify(x => x.Search(It.IsAny<ISpecification<User>>()), Times.Once());
+        }
+    }
+}
---
+++ Sat.Recruitment.Src/Users/Users.UnitTest/Application/Commands/Create/DataProvider.cs
@@ -0,0 +1,43 @@
+﻿using Users.Application.Commands.Create;
+using Users.Domain;
+
+namespace Users.UnitTest.Application.Commands.Create
+{
+    public static class DataProvider
+    {
+        public static CreateUserCommand ValidCommand() => new()
+        {
+            Name = "Mike",
+            Email = new("mike@gmail.com"),
+            Address = "Av. Juan G",
+            Phone = new("+349 1122354215"),
+            UserType = UserType.Normal,
+            Money = 124
+        };
+
+        public static IEnumerable<object[]> InvalidCommands()
+        {
+            yield return new object[] { EmptyCommand() };
+            yield return new object[] { EmptyNameCommand() };
+            yield return new object[] { EmptyAddressCommand() };
+        }
+
+        private static CreateUserCommand EmptyCommand() => new();
+
+        private static CreateUserCommand EmptyNameCommand()
+        {
+            var command = ValidCommand();
+            command.Name = String.Empty;
+
+            return command;
+        }
+
+        private static CreateUserCommand EmptyAddressCommand()
+        {
+            var command = ValidCommand();
+            command.Address = String.Empty;
+
+            return command;
+        }
+    }
+}
