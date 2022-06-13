226a8857c72c54cb3db1ab5d0ef2cfe57588d7af
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 11:40:53 -0300

    new repository exception

---
+++ Sat.Recruitment.Src/Shared/Shared.Domain/Exceptions/RepositoryException.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Domain.Exceptions
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException(string? message) : base(message)
+        { }
+
+        public RepositoryException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
