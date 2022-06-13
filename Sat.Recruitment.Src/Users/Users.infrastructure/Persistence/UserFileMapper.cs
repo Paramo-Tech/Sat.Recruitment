e786be8873e6c5967f535bc23182b7d3f05d807a
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 17:09:52 -0300

    fix user mapping

--- Sat.Recruitment.Src/Users/Users.infrastructure/Persistence/UserFileMapper.cs
+++ Sat.Recruitment.Src/Users/Users.infrastructure/Persistence/UserFileMapper.cs
@@ -4,15 +4,21 @@ namespace Users.infrastructure.Persistence
 {
     public static class UserFileMapper
     {
-        public static User ToUser(string line) => new()
+        public static User ToUser(string line)
         {
-            Name = line.Split(',')[0].ToString(),
-            Email = new(line.Split(',')[1].ToString()),
-            Phone = new(line.Split(',')[2].ToString()),
-            Address = line.Split(',')[3].ToString(),
-            UserType = UserType.FromValue(line.Split(',')[4].ToString()),
-            Money = decimal.Parse(line.Split(',')[5].ToString()),
-        };
+            var userProperties = line.Split(',');
+
+            return new User ()
+            {
+                Name = userProperties[0].ToString(),
+                Email = new(userProperties[1].ToString()),
+                Phone = new(userProperties[2].ToString()),
+                Address = userProperties[3].ToString(),
+                UserType = UserType.FromValue(userProperties[4].ToString()),
+                Money = decimal.Parse(userProperties[5].ToString()),
+            };
+        }
+
 
         public static string ToLine(User user) => 
             $"{user.Name},{user.Email.Value},{user.Phone.Value},{user.Address},{user.UserType.Value},{user.Money}";
