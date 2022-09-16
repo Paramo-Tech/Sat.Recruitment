e8df634c0ae88076c0dcefb0a3b445375728d5ae
Diego Graselli <dgraselli@gmail.com>
2022-09-16 17:51:16 -0300

    Add Model, AppBL, DataService

---
+++ Sat.Recruitment.Api/BusinessLogic/Exceptions/EDuplicatedUserException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sat.Recruitment.Api.BusinessLogic.Exceptions
+{
+    public class EDuplicatedUserException : EVisibleException
+    {
+        public EDuplicatedUserException(string msg) : base($"Duplicated User ({msg})") { }
+    }
+}
---
+++ Sat.Recruitment.Api/BusinessLogic/Exceptions/EVisibleException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sat.Recruitment.Api.BusinessLogic.Exceptions
+{
+    public class EVisibleException : Exception
+    {
+        public EVisibleException(string msg) : base(msg) { }
+    }
+}
