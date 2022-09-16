e258dad023f6cf21cd7694e14c73470d3ea3aaeb
Diego Graselli <dgraselli@gmail.com>
2022-09-16 17:51:58 -0300

    Add tests

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Dynamic;
 
 using Microsoft.AspNetCore.Mvc;
-
+using Moq;
+using Sat.Recruitment.Api.BusinessLogic;
+using Sat.Recruitment.Api.BusinessLogic.Exceptions;
+using Sat.Recruitment.Api.BusinessLogic.Model;
 using Sat.Recruitment.Api.Controllers;
-
+using Sat.Recruitment.Api.Data;
 using Xunit;
 
 namespace Sat.Recruitment.Test
@@ -12,28 +16,85 @@ namespace Sat.Recruitment.Test
     [CollectionDefinition("Tests", DisableParallelization = true)]
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        Mock<IDataService> _dataServiceMock = new Mock<IDataService>();
+        IApplicationBL _applicationBL;
+
+        public UnitTest1()
         {
-            var userController = new UsersController();
+            _dataServiceMock.Setup(x => x.GetUsers()).Returns(GetUsers());
 
-            var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
+            _applicationBL = new ApplicationBL(_dataServiceMock.Object);
 
-
-            Assert.Equal(true, result.IsSuccess);
-            Assert.Equal("User Created", result.Errors);
         }
 
-        [Fact]
-        public void Test2()
+        private List<User> GetUsers()
         {
-            var userController = new UsersController();
+            var _users = new List<User>();
+            _users.Add(new User() { Name = "Oscar Wilde", Email = "owilde@gmail.com", Phone = "0101", Address = "Dublin", Money = 10 });
+            _users.Add(new User() { Name = "Julio Cortazar", Email = "jcortazar@gmail.com", Phone = "0101", Address = "Bruselas", Money = 10 });
 
-            var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
-
-
-            Assert.Equal(false, result.IsSuccess);
-            Assert.Equal("The user is duplicated", result.Errors);
+            return _users;
         }
+
+        [Theory]
+        [InlineData("Oscar Wilde", "test@t.com", "1231", "Dublin")]
+        [InlineData("JC", "jcortazar@gmail.com", "1234", "test")]
+        [InlineData("test", "test@gmail.com", "0101", "test")]
+        public void ShouldThrowEDuplicateUserException(string name, string email, string phone, string addresss)
+        {
+            var user = new User() { Name = name, Email = email, Phone = phone, Address = addresss };
+            Action act = () => _applicationBL.SaveUser(user);
+            Assert.Throws<EDuplicatedUserException>(act);
+        }
+
+        [Theory]
+        [InlineData("Oscar F. Wilde", "test@t.com", "1231", "Dublin")]
+        [InlineData("JC", "jcortazar2@gmail.com", "1234", "test")]
+        [InlineData("test", "test@gmail.com", "01012", "test")]
+        public void ShouldNotThrowEDuplicateUserException(string name, string email, string phone, string addresss)
+        {
+            var user = new User() { Name = name, Email = email, Phone = phone, Address = addresss };
+            Action act = () => _applicationBL.SaveUser(user);
+            var exception = Record.Exception(act);
+            Assert.Null(exception);
+        }
+
+        [Theory]
+        [InlineData("Normal", 101, .12)]
+        [InlineData("Normal", 120, .12)]
+        [InlineData("Normal", 1200,.12)]
+        [InlineData("Normal", 11, .80)]
+        [InlineData("Normal", 50, .80)]
+        [InlineData("Normal", 99, .80)]
+        [InlineData("Normal", 0, 0)]
+        [InlineData("Normal", 1, 0)]
+        [InlineData("Normal", 9, 0)]
+        [InlineData("SuperUser", 101, .20)]
+        [InlineData("Superuser", 200, .20)]
+        [InlineData("SuperUser", 1500, .20)]
+        [InlineData("SuperUser", 20, 0)]
+        [InlineData("SuperUser", 41, 0)]
+        [InlineData("SuperUser", 99, 0)]
+        [InlineData("SuperUser", 0, 0)]
+        [InlineData("SuperUser", 1, 0)]
+        [InlineData("SuperUser", 9, 0)]
+        [InlineData("Premium", 101,  2)]
+        [InlineData("Premium", 200,  2)]
+        [InlineData("Premium", 1500, 2)]
+        [InlineData("Premium", 20, 0)]
+        [InlineData("Premium", 41, 0)]
+        [InlineData("Premium", 99, 0)]
+        [InlineData("Premium", 0, 0)]
+        [InlineData("Premium", 1, 0)]
+        [InlineData("Premium", 9, 0)]
+        public void ShouldGiftBeCorrect(string userType, decimal money, decimal percent)
+        {
+            var user = new User() { Name = "n1", Email = "e1", Phone = "01", Address = "a1", UserType = userType, Money = money };
+            _applicationBL.SaveUser(user);
+            Assert.Equal(money + money * percent , user.Money);
+        }
+
+
+
     }
 }
