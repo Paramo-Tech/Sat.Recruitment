723140ac9bbb9fb4d3c037fa1eed7e60e2a713e6
Sergio Demarco <sergiodemarco@selehann.com>
2023-02-28 21:54:52 -0500

    Cleaning code

--- Sat.Recruitment.Test/UnitTest.cs
+++ Sat.Recruitment.Test/UnitTest.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Dynamic;
-
-using Microsoft.AspNetCore.Mvc;
-
 using Sat.Recruitment.Api.Controllers;
-
 using Xunit;
 
 namespace Sat.Recruitment.Test
 {
     [CollectionDefinition("Tests", DisableParallelization = true)]
-    public class UnitTest1
+    public class UnitTest
     {
         [Fact]
         public void Test1()
@@ -20,7 +14,7 @@ namespace Sat.Recruitment.Test
             var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
 
-            Assert.Equal(true, result.IsSuccess);
+            Assert.True(result.IsSuccess);
             Assert.Equal("User Created", result.Errors);
         }
 
@@ -32,7 +26,7 @@ namespace Sat.Recruitment.Test
             var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
 
-            Assert.Equal(false, result.IsSuccess);
+            Assert.False(result.IsSuccess);
             Assert.Equal("The user is duplicated", result.Errors);
         }
     }
