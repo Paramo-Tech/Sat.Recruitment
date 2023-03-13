2f36bc103d5744a9bef1208d209a85ff6cf3a9d2
Enrique Ortuno <enrique@ortuno.net>
2023-03-13 00:27:10 -0400

    Gift calculation test cases added

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -55,7 +55,7 @@ public class UnitTest1
     {
         var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "333", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Normal" };
         var expectedUser = new User(new UserName(request.Name), new Email(request.Email), new Address(request.Address), new Phone(request.Phone), new UserType(request.UserType), new Money(request.Money));
-        this.Repository.Setup(r => r.SearchBy(It.IsAny<Func<User, bool>>())).ReturnsAsync((User)null); // The user already exists
+        this.Repository.Setup(r => r.SearchBy(It.IsAny<Func<User, bool>>())).ReturnsAsync((User)null); // The user don't exists
 
         var response = await creator.Execute(request);
 
@@ -63,5 +63,75 @@ public class UnitTest1
         Assert.Equal(request.Email, response.Email);
         Assert.Equal(request.Phone, response.Phone);
         Assert.Equal(request.Address, response.Address);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_NormalUser_MoneyAbove100()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "101", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Normal" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(113.12m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_NormalUser_MoneyLess100AndGreaterThan10()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "80", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Normal" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(144m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_NormalUser_MoneyLess10()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "8", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Normal" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(8m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_SuperUser_Above100()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "101", Name = "Jorge Paz", Phone = "+59182345678", UserType = "SuperUser" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(121.2m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_SuperUser_Below100()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "99", Name = "Jorge Paz", Phone = "+59182345678", UserType = "SuperUser" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(99m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_PremiumUser_Above100()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "120", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Premium" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(360m, response.Money);
+    }
+
+    [Fact]
+    public async Task ItShould_CalculateGit_OK_PremiumUser_Below100()
+    {
+        var request = new UserRequest() { Address = "4th Avenue 5567", Email = "jorge@lasalle.org", Money = "60", Name = "Jorge Paz", Phone = "+59182345678", UserType = "Premium" };
+
+        var response = await creator.Execute(request);
+
+        Assert.Equal(60m, response.Money);
     }
 }
