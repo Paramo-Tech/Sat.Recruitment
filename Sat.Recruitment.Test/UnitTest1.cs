b786e35c77a51c9ecd514ddf52694a1fd552433f
Enrique Ortuno <enrique@ortuno.net>
2023-03-12 22:23:59 -0400

    test project upgraded and ports restored

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -1,39 +1,10 @@
-using System;
-using System.Dynamic;
+﻿namespace Sat.Recruitment.Test;
 
-using Microsoft.AspNetCore.Mvc;
-
-using Sat.Recruitment.Api.Controllers;
-
-using Xunit;
-
-namespace Sat.Recruitment.Test
+[TestClass]
+public class UnitTest1
 {
-    [CollectionDefinition("Tests", DisableParallelization = true)]
-    public class UnitTest1
+    [TestMethod]
+    public void TestMethod1()
     {
-        [Fact]
-        public void Test1()
-        {
-            var userController = new UsersController();
-
-            var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
-
-
-            Assert.Equal(true, result.IsSuccess);
-            Assert.Equal("User Created", result.Errors);
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            var userController = new UsersController();
-
-            var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
-
-
-            Assert.Equal(false, result.IsSuccess);
-            Assert.Equal("The user is duplicated", result.Errors);
-        }
     }
 }
