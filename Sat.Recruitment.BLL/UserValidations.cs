cb1c934f8963b6ee0ebab32bc6e6b4ff05674f73
leo_rivflo <leo.rivera@rank.com>
2023-03-26 17:09:32 -0600

    Paramo Tech Exam by Leonardo Rivera

---
+++ Sat.Recruitment.BLL/UserValidations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Sat.Recruitment.BLL
+{
+    public class UserValidations
+    {
+        public static decimal GetUserTypeMoney(string userType, string moneyAmount, decimal newUserMoney)
+        {
+            decimal bonusAmount = 0;
+
+            decimal money = decimal.Parse(moneyAmount);
+
+            if (userType == "Normal")
+            {
+                bonusAmount = GetNormalUserBonusAmount(money);
+            }
+            else if (userType == "SuperUser")
+            {
+                bonusAmount = GetSuperUserBonusAmount(money);
+            }
+            else if (userType == "Premium")
+            {
+                bonusAmount = GetPremiumUserBonusAmount(money);
+            }
+
+            return newUserMoney + bonusAmount;
+        }
+
+        public static string NormalizeEmail(string email)
+        {
+            var aux = email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
+
+            aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
+
+            return string.Join("@", new string[] { aux[0], aux[1] });
+        }
+
+        private static decimal GetNormalUserBonusAmount(decimal moneyAmount)
+        {
+            if (moneyAmount > 100)
+            {
+                decimal percentage = 0.12m;
+                return moneyAmount * percentage;
+            }
+            else if (moneyAmount > 10)
+            {
+                decimal percentage = 0.8m;
+                return moneyAmount * percentage;
+            }
+
+            return 0;
+        }
+
+        private static decimal GetSuperUserBonusAmount(decimal moneyAmount)
+        {
+            if (moneyAmount > 100)
+            {
+                decimal percentage = 0.20m;
+                return moneyAmount * percentage;
+            }
+
+            return 0;
+        }
+
+        private static decimal GetPremiumUserBonusAmount(decimal moneyAmount)
+        {
+            if (moneyAmount > 100)
+            {
+                return moneyAmount * 2;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Sat.Recruitment.BO/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sat.Recruitment.BO
+{
+    public class Response
+    {
+        public class ResponseList
+        {
+            public int Id { get; set; }
+            public string Error { get; set; }
+        }
+    }
+}
