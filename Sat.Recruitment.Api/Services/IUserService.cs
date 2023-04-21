263ddb21abeb588062d543c56356b86de23f80c3
sprado <santiagojprado@gmail.com>
2023-04-21 01:16:23 -0300

    add userservice interface

---
+++ Sat.Recruitment.Api/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Sat.Recruitment.Api.Models;
+using System.Collections.Generic;
+
+namespace Sat.Recruitment.Api.Services
+{
+    public interface IUserService
+    {
+        bool Create(User entity);
+        IList<User> GetAll();
+    }
+}
