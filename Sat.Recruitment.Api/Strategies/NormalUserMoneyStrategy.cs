e1de008a2338c514da0e8686d52b77a0965efc85
sprado <santiagojprado@gmail.com>
2023-04-20 23:10:00 -0300

    Add usertype enum add money strategy

---
+++ Sat.Recruitment.Api/Strategies/NormalUserMoneyStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Api.Strategies
+{
+    public class NormalUserMoneyStrategy : IUserMoneyStrategy
+    {
+        public decimal CalculateAdditionalMoney(decimal originalMoney)
+        {
+            decimal additionalMoney = 0;
+
+            if (originalMoney > 100)
+            {
+                additionalMoney = originalMoney * 0.12m;
+            }
+            else if (originalMoney > 10)
+            {
+                additionalMoney = originalMoney * 0.8m;
+            }
+
+            return additionalMoney;
+        }
+    }
+
+    
+}
