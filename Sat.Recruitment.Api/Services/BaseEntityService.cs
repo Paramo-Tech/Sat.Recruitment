6be8d1ed73f88a423fd677337b6acdc406b9d608
sprado <santiagojprado@gmail.com>
2023-04-21 00:47:50 -0300

    base service and user service

---
+++ Sat.Recruitment.Api/Services/BaseEntityService.cs
@@ -0,0 +1,34 @@
+﻿using Sat.Recruitment.Api.Helpers;
+using System.Collections.Generic;
+
+namespace Sat.Recruitment.Api.Services
+{
+    public abstract class BaseEntityService<Entity> where Entity : new()
+    {
+        private FileHelper _fileHelper;
+        protected abstract string FileName { get; }
+
+        public BaseEntityService()
+        {
+            _fileHelper = new FileHelper(FileName);
+        }
+
+        public abstract Entity FormatEntity(string[] lines);
+
+        public abstract bool Create(Entity entity);
+
+
+        public virtual IList<Entity> GetAll()
+        {
+            var entities = new List<Entity>();
+            var lines = _fileHelper.ReadFromFile();
+            foreach (var line in lines)
+            {
+                string[] fields = line.Split(',');
+                entities.Add(FormatEntity(fields));
+            }
+
+            return entities;
+        }
+    }
+}
